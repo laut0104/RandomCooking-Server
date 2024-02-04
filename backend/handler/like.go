eefcf765f5b052158474171720522caabc9be866
Miyu Yano <78589722+laut0104@users.noreply.github.com>
2024-02-04 19:52:34 +0900

    メニューをいいねできるようにする

---
+++ backend/handler/like.go
@@ -0,0 +1,59 @@
+package handler
+
+import (
+	"github.com/laut0104/RandomCooking/domain/entity"
+	usecase "github.com/laut0104/RandomCooking/usecase/interactor"
+	_ "github.com/lib/pq"
+
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type LikeHandler struct {
+	likeUC *usecase.LikeUseCase
+}
+
+func NewLikeHandler(likeUC *usecase.LikeUseCase) *LikeHandler {
+	return &LikeHandler{likeUC: likeUC}
+}
+
+func (h *LikeHandler) GetMenuByUniqueKey(c echo.Context) error {
+	userID := c.Param("uid")
+	menuID := c.Param("mid")
+	menus, err := h.likeUC.GetLikeByUniqueKey(userID, menuID)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return c.JSON(http.StatusOK, menus)
+}
+
+func (h *LikeHandler) AddLike(c echo.Context) error {
+	var like *entity.Like
+
+	if err := c.Bind(&like); err != nil {
+		log.Println(err)
+		return err
+	}
+
+	if err := h.likeUC.AddLike(like); err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return c.JSON(http.StatusOK, like)
+}
+
+func (h *LikeHandler) DeleteLike(c echo.Context) error {
+	id := c.Param("id")
+
+	if err := h.likeUC.DeleteLike(id); err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return c.NoContent(http.StatusOK)
+}
